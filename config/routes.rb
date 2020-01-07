e9d8aceb9dac37dccd3c8b9888151f4b4fd7026d
Edward Jacobs <eddielawrence23@gmail.com>
2020-01-07 15:20:54 -0600

    Add the doses controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'doses/new'
-  get 'doses/create'
-  get 'cocktails/index'
-  get 'cocktails/new'
-  get 'cocktails/create'
-  get 'cocktails/show'
-
+  # get 'doses/new'
+  # get 'doses/create'
+  # get 'cocktails/index'
+  # get 'cocktails/new'
+  # get 'cocktails/create'
+  # get 'cocktails/show'
+  root to: "cocktails#index"
   resources :cocktails, only: [:index, :show, :new, :create] do
-    resources :doses, only: [:new, :create]
+    resources :doses, only: [:create]
   end
   resources :doses, only: [:destroy]
 end
