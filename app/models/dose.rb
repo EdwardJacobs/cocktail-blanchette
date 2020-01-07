a8b02ab495f094c29f89e7385177d8e0cc61211a
Edward Jacobs <eddielawrence23@gmail.com>
2020-01-07 17:00:49 -0600

    add images, scss, and divs to index page

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,6 +1,6 @@
 class Dose < ApplicationRecord
-  belongs_to :ingredients
-  belongs_to :cocktails
+  belongs_to :ingredient
+  belongs_to :cocktail
   validates_uniqueness_of :cocktail_id, scope: :ingredient_id
 
   validates :description, presence: true
