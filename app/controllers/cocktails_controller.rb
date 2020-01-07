13f5cd620d2815633aba7de9d3dd78af8ebdf522
Edward Jacobs <eddielawrence23@gmail.com>
2020-01-06 22:25:31 -0600

    Create cocktails controller and basic new and show views

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -3,6 +3,20 @@ class CocktailsController < ApplicationController
     @cocktails = Cocktail.all
   end
 
+  def new
+    @cocktail = Cocktail.new
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
   def show
     @cocktail = Cocktail.find(params[:id])
     @dose = Dose.new
